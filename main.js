7a9910d9c9b008e30a01113740b3bda9bd679475
Htain Linn Htoo <phohtoo@gmail.com>
2022-06-13 23:29:47 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import blog, { ga, redirects } from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "Linn Htoo's Notes",
+  author: "Linn Htoo",
+  avatar: "./avatar.jpg",
+  avatarClass: "full",
+  links: [
+    { title: "Email", url: "mailto:htain@linnhtoo.com" },
+    { title: "GitHub", url: "https://github.com/phohtoo" },
+  ],
+  background: "#f9f9f9",
+  middlewares: [
+    ga("UA-82068495-1"),
+    redirects({
+      "iocp-links.html": "iocp_links",
+      "rant.html": "rant",
+    }),
+  ],
+});
